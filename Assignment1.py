46349b91201ad3fd057975645464f926c6382110
nida jabbar <74002695+Nidajabbar05@users.noreply.github.com>
2022-01-01 19:29:24 +0500

    Assigment#1

---
+++ Assignment1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print( "Twinkle twinkle little star")
+print(" How I wonder what you Are")
+print("     Up Above the world so high")
+print("     Like a diamond in the sky")
+print( "Twinkle twinkle little star")
+print(" How I wonder what you Are")
+
+
+# In[6]:
+
+
+import platform
+print(platform.python_version())
+
+
+# In[10]:
+
+
+import datetime
+now = datetime.datetime.now();
+print("current_date and time : " + now.strftime( "%Y-%M-%D %H:%M:%S"));
+
+
+# In[14]:
+
+
+firstname = input("Input your name:")
+lastname = input("Input your last name:");
+print(lastname +" "+ firstname);                  
+
+
+# In[18]:
+
+
+num1 = input("Enter first value?");
+num2 = input("Enter second value");
+result = int(num1) + int(num2);
+print("Final result : " + str(result));
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
